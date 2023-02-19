76cd025f4839632111b39fb65fe833cac075cdbb
smbloodworth <sarah.bloodworth@gmail.com>
2023-02-19 16:06:18 -0500

    updates

--- assignments/assignment3/Calculator.php
+++ assignments/assignment3/Calculator.php
@@ -3,50 +3,49 @@
 class Calculator {
 
     public function calc($operator, $firstNumber=1, $secondNumber=2){
-        if (is_string($operator) && (is_int($firstNumber) || is_float($firstNumber)) && (is_int($secondNumber) || is_float($secondNumber))) { // Checks for up to three arguments, then checks for types string, number, number
+        if (is_string($operator) && (is_int($firstNumber) || is_float($firstNumber)) && (is_int($secondNumber) || is_float($secondNumber))) { // Checks for up to three arguments, then checks for types: string, number, number
+           
+            if ($firstNumber !== null && $secondNumber !== null) { //ATTEMPTING TO CATCH TOO FEW ARGUMENTS ENTERED
 
-            if ($operator === "+") {
-                add($firstNumber, $secondNumber); // Calls for addition logic
-            } else if ($operator === "-") {
-                subtract($firstNumber, $secondNumber); // Calls for subtraction logic
-            } else if ($operator === "*") {
-                multiply($firstNumber, $secondNumber); // Calls for multiplication logic
-            } else if ($operator === "/") {
-                divide($firstNumber, $secondNumber); // Calls for division logic
-            } else { // Catches if a string other than +, -, *, or / is entered
-                return "You must enter a string and two numbers";
+                 if ($operator === "+") {
+                    echo $this->add($firstNumber, $secondNumber); // Calls for addition logic
+                } else if ($operator === "-") {
+                    echo $this->subtract($firstNumber, $secondNumber); // Calls for subtraction logic
+                } else if ($operator === "*") {
+                    echo $this->multiply($firstNumber, $secondNumber); // Calls for multiplication logic
+                } else if ($operator === "/") {
+                    echo $this->divide($firstNumber, $secondNumber); // Calls for division logic
+                } else { // Catches if a string other than +, -, *, or / is entered
+                    return "You must enter a string and two numbers"."<br>";
+                }
             }
-
         } else { // Triggers for any bad arguments for either numbers
-            return "You must enter a string and two numbers"; 
+            return "You must enter a string and two numbers"."<br>"; 
         }
     }
 
 
-    //NEED TO FIGURE OUT WHY THESE AREN'T BEING CALLED RIGHT  :[
-
-
-    function add($firstNumber, $secondNumber) {
+    function add($firstNumber, $secondNumber) { // Actual addition happening here
         $answer = ($firstNumber + $secondNumber);
-        return "The sum of the numbers is {$answer}";
+        return "The sum of the numbers is {$answer}"."<br>";
     }
 
-    function subtract($firstNumber, $secondNumber) {
+    function subtract($firstNumber, $secondNumber) { // Actual subtraction happening here
         $answer = ($firstNumber - $secondNumber);
-        return "The difference of the numbers is {$answer}";
+        return "The difference of the numbers is {$answer}"."<br>";
     }
 
-    function multiply($firstNumber, $secondNumber) {
+    function multiply($firstNumber, $secondNumber) { // Actual multiplication happening here
         $answer = ($firstNumber * $secondNumber);
-        return "The product of the numbers is {$answer}";
+        return "The product of the numbers is {$answer}"."<br>";
     }
 
-    function divide($firstNumber, $secondNumber) {
+    function divide($firstNumber, $secondNumber) { // Actual division happening here
         if ($secondNumber != 0) {
             $answer = ($firstNumber / $secondNumber);
-            return "The division of the numbers is {$answer}";
-        } else {
-            return "Cannot divide by zero";
+            return "The division of the numbers is {$answer}"."<br>" ;
+        } else { // Catches any attempt to divide by zero
+            return "Cannot divide by zero"."<br>";
         }
     }
 }
